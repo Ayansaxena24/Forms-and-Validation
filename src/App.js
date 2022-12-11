c2cc723f183c18526da991081288de482d00cc95
Ayansaxena24 <ayan.saxena24@gmail.com>
2022-12-11 15:38:25 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { useForm } from "react-hook-form"; 
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const { register, handleSubmit, errors} = useForm();
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log(data);
+  };
+  console.log(errors);
+  return (
+    <div className = "container">
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={() => handleSubmit(onSubmit)}> 
+        <h1>Registration Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input 
+            type="text" 
+            name="username" 
+            placeholder="Username" 
+            {...register("message",{ required: "Username is required", })}
+            />
+          </div>
+          <p>{errors?.email?.message}</p>
+          <div className="field">
+            <label>Email</label>
+            <input 
+            type="email" 
+            name="email" 
+            placeholder="Email" 
+            {...register("message",
+            { required: "Email is required",
+              pattern: 
+              {
+                value: /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g, 
+                message:"This is not a valid email"
+              }
+            }
+            )}/>
+           <p>{errors?.email?.message}</p>
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input 
+            type="password" 
+            name="Password" 
+            placeholder="Password" 
+            {...register("message",
+            { required: "Password is required",
+              minLength:
+              {
+                value:4,
+                message:"Password must contain more than 4 characters"},
+              maxLength:
+              {
+                value:10,
+                message:"Password cannot contain more than 10 words"
+              },
+            }
+            )}/>
+           <p>{errors?.email?.message}</p>
+          </div>
+          <button className='sbmt-btn'>Submit</button>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+export default App
