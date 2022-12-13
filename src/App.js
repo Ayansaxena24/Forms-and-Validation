2bd93a4355dd642c8693375eeb61acffba7db2ae
Ayansaxena24 <ayan.saxena24@gmail.com>
2022-12-14 00:53:56 +0530

    removing g from value

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
             { required: "Email is required",
               pattern: 
               {
-                value: /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g, 
+                value: /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/, 
                 message:"This is not a valid email"
               }
             }
